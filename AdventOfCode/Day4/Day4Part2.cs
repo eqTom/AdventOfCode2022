bf3bd92098d68e7544e0441fdc5b55e7cf88457e
Tom Sharkey <tom@sharkey.im>
2022-12-09 16:17:22 +0000

    day 4 part 2

--- AdventOfCode/Day4/Day4Part2.cs
+++ AdventOfCode/Day4/Day4Part2.cs
@@ -3,6 +3,20 @@
     public override int Run()
     {
         int score = 0;
+        foreach (string line in File.ReadLines(@"./data/day4.txt"))
+        {
+            var assignments = line.Split(',');
+            var elf1 = StartEnd(assignments[0]);
+            var elf2 = StartEnd(assignments[1]);
+            if (elf1.Item2 >= elf2.Item1 && elf1.Item1 <= elf2.Item2)
+                score++;
+        }
         return score;
+    }
+
+    private (int, int) StartEnd(string input)
+    {
+        var s = input.Split('-');
+        return (int.Parse(s[0]), int.Parse(s[1]));
     }
 }
